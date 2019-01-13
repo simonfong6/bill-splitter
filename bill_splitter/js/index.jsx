0cabee805a53923458a3d19514fe628ea1ee256f
Simon <simonfong6@gmail.com>
2019-01-12 23:50:39 -0800

    Renamed index.jsx to captial

--- bill_splitter/js/index.jsx
+++ bill_splitter/js/index.jsx
@@ -9,19 +9,10 @@ class Index extends React.Component {
 
     return (
       <div>
-        <NavBar/>
-        <Banner/>
         <h1
-          className='text-center'
-          style={{
-            marginBottom: '1em'
-          }}
-        >
-          Welcome to the FUSION Hip-Hop Dance Competition website!
+          className="text-center">
+          Bill Splitter
         </h1>
-        <EventsUpcoming/>
-        <ButtonTicket/>
-        <Footer/>
       </div>
       
     );
